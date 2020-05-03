1fa74f338d7c3ef73f637e3329e6b04ec634c514
kevin olson <acidjazz@gmail.com>
2020-05-02 19:05:01 -0500

    ⬆️ dependency updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .container.lg
     .flex.justify-around.p-6
-      .font-bold.py-2.px-6.text-lg kso
+      .font-bold.py-2.px-6.text-lg kevin olson
       .flex
         .mx-2.py-2.px-6.relative.w-24
           MenuItem(name="cover")
